24a347bcddb502c655bd127978cc15d779f3b0df
Ian <russmoonbase@gmail.com>
2020-11-13 09:14:25 -0700

    Fixed textures on ladder // added elevator switch that turns green when pressed

---
+++ Assets/Scripts/Elevator/ElevatorPanel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorPanel : MonoBehaviour
+{
+   [SerializeField] private Light _pointLight;
+
+   private void OnTriggerStay(Collider other)
+   {
+      if (other.tag == "Player")
+      {
+         if (Input.GetKeyDown(KeyCode.P))
+         {
+            _pointLight.color = Color.green;
+         }
+         
+      }
+   }
+}
