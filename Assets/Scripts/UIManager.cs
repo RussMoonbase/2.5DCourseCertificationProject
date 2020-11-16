4603a2a215db5b38a8173b23bbe866394e7e8d08
Ian <russmoonbase@gmail.com>
2020-11-16 08:07:23 -0700

    Last push of the project // Added basic UI for coins and a collectable prefab

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+   public static UIManager instace;
+   [SerializeField] private TMP_Text _coinAmountText;
+
+   private void Awake()
+   {
+      instace = this;
+   }
+
+   // Start is called before the first frame update
+   void Start()
+   {
+      _coinAmountText.text = "0";
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+
+   }
+
+   public void UpdateCoinAmount(int newAmount)
+   {
+      if (_coinAmountText)
+      {
+         _coinAmountText.text = newAmount.ToString();
+      }
+      
+   }
+}
